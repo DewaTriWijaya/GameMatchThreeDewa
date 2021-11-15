9e8afb9a4abda14d8eb94f90d5717a50d0e3e30d
Dewa <73357308+DewaTriWijaya@users.noreply.github.com>
2021-11-15 21:01:11 +0700

    Add project to github

---
+++ Assets/Script/GameFlowManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class GameFlowManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static GameFlowManager _instance = null;
+
+    public static GameFlowManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<GameFlowManager>();
+
+                if(_instance == null)
+                {
+                    Debug.LogError("Fata Error: GameFlowManager not found");
+                }
+            }
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    [Header("UI")]
+    public UIGameOver GameOverUI;
+
+    public bool IsGamerOver { get { return isGamerOver; } }
+
+    private bool isGamerOver = false;
+
+    private void Start()
+    {
+
+        isGamerOver = false;
+    }
+
+
+    public void GameOver()
+    {
+        isGamerOver = true;
+        ScoreManager.Instance.setHighScore();
+        GameOverUI.Show();
+    }
+}
